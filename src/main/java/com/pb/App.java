7e1c7a13405b3e824aa27bed99178afd5d5dd1e7
Bardas Oleksandr <iskander0119@gmail.com>
2019-05-26 01:45:38 +0300

    commit

--- src/main/java/com/pb/App.java
+++ src/main/java/com/pb/App.java
@@ -7,6 +7,20 @@ import org.apache.logging.log4j.Logger;
 
 public class App {
 
+//    Конфигурирование Log4j2
+//    Конфигурационные файлы будут искаться в classpath.
+//    Порядок просмотра файлов при поиске конфигурационных настроек (поиск осуществляется вероятно, до первого найденного файла):
+//            log4j.configurationFile (в документации этот файл обозначен как system property)
+//    log4j2-test.properties
+//    log4j2-test.yaml or log4j2-test.yml
+//    log4j2-test.json or log4j2-test.jsn
+//    log4j2-test.xml
+//    log4j2.properties
+//    log4j2.yaml or log4j2.yml
+//    log4j2.json or log4j2.jsn
+//    log4j2.xml
+//    DefaultConfiguration – используется если не будет найдено ни одного файла, при этом вывод будет осуществляться на консоль
+
     private static final Logger LOG = LogManager.getLogger(App.class);
 
     public static void main(String[] args) {
